6df91ec1c0adcdf74c21f82005d5934ec5009fc6
Gavin Segal <gsegal@ford.com>
2025-09-10 14:53:40 -0400

    adjusting region

--- environments/dev/scheduler.tf
+++ environments/dev/scheduler.tf
@@ -2,7 +2,7 @@
 # A resource that can be linked to a compute instance to keep it only up during the day
 resource "google_compute_resource_policy" "conservative" {
   name        = "gce-policy"
-  region      = gcp.region
+  region      = var.gcp.region
   description = "Start and stop instances"
   instance_schedule_policy {
     vm_start_schedule {
